4cf639d31d8ae8eae8cac55c7a36efae30525698
Andrew Capp <acapp909@gmail.com>
2019-09-22 23:37:48 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def update_counting_hash(hash, key)
    {key => hash[key] +=1}
  else
    puts "no"
-   update_counting_hash = {key => 1}
+   update_counting_hash = hash{key => 1}
  end
 end
 update_counting_hash(hash1, :names)
