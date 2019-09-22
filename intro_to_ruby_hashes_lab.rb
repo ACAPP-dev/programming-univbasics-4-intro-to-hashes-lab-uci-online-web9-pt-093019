c224ee4b44899b87cf5aa077e451cc827790d415
Andrew Capp <acapp909@gmail.com>
2019-09-22 23:35:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,12 +27,12 @@ def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
   read_from_hash = hash[key]
 end
-
+hash1 = {:name => 1, :age => 5}
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
-  hash1 = {:name => 1, :age => 5}
+  
   if hash[key]
    # update_counting_hash = {key => hash[key] +=1}
    puts "Yes"
