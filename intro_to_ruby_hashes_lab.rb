94ae25785c1f702a2448a4624c2fbc889cc5f68e
Andrew Capp <acapp909@gmail.com>
2019-09-22 22:46:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,10 +5,12 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
+  my_hash = {one: "Test"}
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {name: "Grace Hopper"}
 end
 
 def id_generator
